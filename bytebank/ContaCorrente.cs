98c4ef417b1ca84fdf25dfbf1e80d0449d58d511
antonioviniciusvb <antonioviniciusvb@outlook.com>
2022-08-25 14:38:16 -0300

    Adicionar arquivos de projeto.

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,115 @@
+﻿namespace Bytebank
+{
+    public class ContaCorrente
+    {
+
+        public ContaCorrente()
+        {
+
+        }
+
+        public ContaCorrente(string titular, int agencia, string nomeDaAgencia, string conta, double saldo)
+        {
+            this.Titular = titular;
+            this.Agencia = agencia;
+            this.NomeDaAgencia = nomeDaAgencia;
+            this.Conta = conta;
+            this.Saldo = saldo;
+        }
+
+        public string Titular { get; set; }
+        public int Agencia { get; set; }
+
+        public string NomeDaAgencia { get; set; }
+
+        public string Conta { get; set; }
+
+        public double Saldo { get; set; }
+
+        /// <summary>
+        /// Método para Saque 
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public bool Sacar(double valorDeSaque)
+        {
+            if (ValidaSaque(valorDeSaque))
+            {
+                Saldo -= valorDeSaque;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Método para Depósito
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
+        public bool Depositar(double valorDeposito)
+        {
+            if (ValidaDeposito(valorDeposito))
+            {
+                Saldo += valorDeposito;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Método de validação de Saque
+        /// </summary>
+        /// <param name="valorDeSaque"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        private bool ValidaSaque(double valorDeSaque)
+        {
+            if (valorDeSaque > Saldo)
+            {
+                throw new Exception($"Valor de {valorDeSaque:c} para Saque é maior que Saldo em Conta Corrente.");
+
+            }
+            else if (EhMenorOuIgualZero(valorDeSaque))
+            {
+                throw new Exception($"Valor de {valorDeSaque:c} para Saque é inválido.");
+            }
+
+            return true;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Titular: {Titular}" + Environment.NewLine +
+                         $"Agência: {Agencia} - {NomeDaAgencia}, conta: {Conta}" + Environment.NewLine +
+                         $"Saldo: {Saldo:c}";
+        }
+
+        /// <summary>
+        /// Método para validação de Depósito
+        /// </summary>
+        /// <param name="valorDeDeposito"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        private bool ValidaDeposito(double valorDeDeposito)
+        {
+            if (EhMenorOuIgualZero(valorDeDeposito))
+            {
+                throw new Exception($"Valor de {valorDeDeposito:c} para Depósto é inválido.");
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Método para retornar value <= 0
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
+        private bool EhMenorOuIgualZero(double valor)
+        {
+            return valor <= 0;
+        }
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,65 @@
+﻿using Bytebank;
+
+ContaCorrente contaCorrente = new ContaCorrente();
+
+contaCorrente.Titular = "Antonio Vinicius";
+contaCorrente.Agencia = 2314;
+contaCorrente.NomeDaAgencia = "Central";
+contaCorrente.Conta = "58144-7";
+contaCorrente.Saldo = 93.50;
+
+ContaCorrente contaCorrente2 = new()
+{
+    Titular = "Ísis de Souza Veras",
+    Agencia = 2314,
+    NomeDaAgencia = "Flórida",
+    Conta = "14526-x",
+    Saldo = 10142.33
+};
+
+Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
+Console.WriteLine("************");
+Console.WriteLine($"Titular: {contaCorrente.Titular}");
+Console.WriteLine($"Agência: {contaCorrente.Agencia} - {contaCorrente.NomeDaAgencia}, conta: {contaCorrente.Conta}");
+Console.WriteLine($"Saldo: {contaCorrente.Saldo:c}");
+
+Console.WriteLine("************");
+Console.WriteLine($"Titular: {contaCorrente2.Titular}");
+Console.WriteLine($"Agência: {contaCorrente2.Agencia} - {contaCorrente2.NomeDaAgencia}, conta: {contaCorrente2.Conta}");
+Console.WriteLine($"Saldo: {contaCorrente2.Saldo:c}");
+
+//Um exemplo usando o String.Format dentro da string interpolada
+//Console.WriteLine($"Saldo: R$ {String.Format("{0:0.00}", contaCorrente2.Saldo)}");
+
+
+//Efetuando Teste de  Saque 
+double valorDoSaque = 100;
+
+Console.WriteLine();
+Console.WriteLine();
+
+try
+{
+   var sacou = contaCorrente.Sacar(valorDoSaque);
+
+    if (sacou)
+    {
+        Console.WriteLine($"Foram sacados {valorDoSaque:c} do Titular: {contaCorrente.Titular}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+
+    Console.WriteLine();
+    Console.WriteLine("****** Conta Corrente ******");
+    Console.WriteLine(contaCorrente.ToString());
+    //Console.WriteLine($"Titular: {contaCorrente.Titular}");
+    //Console.WriteLine($"Agência: {contaCorrente.Agencia} - {contaCorrente.NomeDaAgencia}, conta: {contaCorrente.Conta}");
+    //Console.WriteLine($"Saldo: {contaCorrente.Saldo:c}");
+}
+
+Console.ReadKey();
