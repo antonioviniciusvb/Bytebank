5025a0086e7d67247199eda6c1ae2b6ec8dae711
antonioviniciusvb <antonioviniciusvb@outlook.com>
2022-08-25 14:58:47 -0300

    Adição da classe Útil com funções simples e genéricas comum entre as outras classes.

---
+++ bytebank/Util.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    public static class Util
+    {
+        public static void PulaLinhas(int saltos = 1)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+            }
+
+            if (saltos > 0)
+            {
+                for (int i = 0; i < saltos; i++)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Método para retornar value <= 0
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
+        public static bool EhMenorOuIgualZero(double valor)
+        {
+            return valor <= 0;
+        }
+    }
+}
