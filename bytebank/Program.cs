83d15ae6547095777e895f8d16ff8f8ee3462401
antonioviniciusvb <antonioviniciusvb@outlook.com>
2022-08-25 21:23:04 -0300

    Atualizações nos testes

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -1,48 +1,59 @@
 ﻿using bytebank;
 using System.Linq.Expressions;
 
-ContaCorrente conta_1 = new()
-{ 
-    Cliente = new(nome: "Antonio Vinicius", cpf: "999.999.999-14", profissao: "Desenvolvedor .NET"),
-    Agencia = 2314,
-    NomeDaAgencia = "Central",
-    Conta = "58144-7",
-    Saldo = 93.50
-};
-
-ContaCorrente conta_2 = new()
-{
-    Cliente = new (nome: "Iris", cpf: "555.555.555-47", profissao: "Desenvolvedor Java"),
-    Agencia = 2314,
-    NomeDaAgencia = "Flórida",
-    Conta = "14526-x",
-    Saldo = -10
-};
-
-Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
-
-Util.PularLinhas(2);
-
-Console.WriteLine(conta_1.ToString());
-
-Util.PularLinhas();
-
-Console.WriteLine(conta_2.ToString());
-
-//Um exemplo usando o String.Format dentro da string interpolada
-//Console.WriteLine($"Saldo: R$ {String.Format("{0:0.00}", conta_2.Saldo)}");
-
-
-
-#region Testes
-
-//Valores para Saque e Depósito
-double valorDoSaque = 50.33;
-double valorDoDeposito = 14000;
-double valorDaTransferencia = 4587.52;
 
 try
 {
+    ContaCorrente conta_1 = new()
+    {
+        Cliente = new(nome: "Antonio Vinicius", cpf: "999.999.999-14", profissao: "Desenvolvedor .NET"),
+        Agencia = 2314,
+        NomeDaAgencia = "Central",
+        Conta = "58144-7",
+        Saldo = 93.50
+    };
+
+    ContaCorrente conta_2 = new()
+    {
+        Cliente = new(nome: "Iris", cpf: "555.555.555-47", profissao: "Desenvolvedor Java"),
+        Agencia = 2314,
+        NomeDaAgencia = "Flórida",
+        Conta = "14526-x",
+        Saldo = 40145.20
+    };
+
+
+    //Teste com conta inválida
+    //ContaCorrente conta_3 = new()
+    //{
+    //    Cliente = new(nome: "Carlos Tortelli", cpf: "533.222.000-82", profissao: "Desenvolvedor Python"),
+    //    Agencia = 4571,
+    //    NomeDaAgencia = "Diadema",
+    //    Conta = "14306-ww",
+    //    Saldo = 40145.20
+    //};
+
+    Console.WriteLine("Boas Vindas ao seu banco, ByteBank!");
+
+    Util.PularLinhas(2);
+
+    Console.WriteLine(conta_1.ToString());
+
+    Util.PularLinhas();
+
+    Console.WriteLine(conta_2.ToString());
+
+    //Um exemplo usando o String.Format dentro da string interpolada
+    //Console.WriteLine($"Saldo: R$ {String.Format("{0:0.00}", conta_2.Saldo)}");
+
+    #region Testes
+
+    //Valores para Saque e Depósito
+    double valorDoSaque = 50.33;
+    double valorDoDeposito = 14000;
+    double valorDaTransferencia = 4587.52;
+
+
     #region Saque
     Util.PularLinhas(2);
 
